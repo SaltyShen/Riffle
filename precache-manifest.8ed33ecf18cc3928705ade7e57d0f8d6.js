27c5dc2a032bd4b33092d0a632e91e62f81da84a
Shen Wang <peterhuiwang1995@gmail.com>
2020-05-14 21:28:09 -0500

    Updates

---
+++ precache-manifest.8ed33ecf18cc3928705ade7e57d0f8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340b9484807c806edf5a03d1ae8ec84a",
+    "url": "/Riffle/index.html"
+  },
+  {
+    "revision": "66081cd41591813b51df",
+    "url": "/Riffle/static/css/main.a396aba5.chunk.css"
+  },
+  {
+    "revision": "bb4858d8b777e0ae3b72",
+    "url": "/Riffle/static/js/2.39fafaa3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Riffle/static/js/2.39fafaa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66081cd41591813b51df",
+    "url": "/Riffle/static/js/main.e1943858.chunk.js"
+  },
+  {
+    "revision": "0084ea5ed7d14274a8d6",
+    "url": "/Riffle/static/js/runtime-main.cdbe08dd.js"
+  },
+  {
+    "revision": "bf2c47e6b6700f3efb088a5751a38c4f",
+    "url": "/Riffle/static/media/logo.bf2c47e6.svg"
+  }
+]);

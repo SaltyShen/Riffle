a8daf0562dbffdb1d37ab5a2788c3d235b335151
Shen Wang <peterhuiwang1995@gmail.com>
2020-07-10 17:15:51 -0500

    Updates

--- precache-manifest.da6bec976f9bd4934e3b2eea9fc727ff.js
+++ precache-manifest.da6bec976f9bd4934e3b2eea9fc727ff.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c7b28944b80f4004b81e48c6a913ebc",
+    "revision": "7ad85cf785f748bc26ddd59ebb9adc74",
     "url": "/Riffle/index.html"
   },
   {
-    "revision": "de1ef51a7ba4317faae6",
+    "revision": "32c5e736c2581e70af35",
     "url": "/Riffle/static/css/main.306dd00d.chunk.css"
   },
   {
-    "revision": "0d96d5f12a5a7b8b8a39",
-    "url": "/Riffle/static/js/2.8cd2fa14.chunk.js"
+    "revision": "29d7d10994eac6c072fa",
+    "url": "/Riffle/static/js/2.7fb3a70c.chunk.js"
   },
   {
     "revision": "ca962714ee3537eb863ad2954c33c63e",
-    "url": "/Riffle/static/js/2.8cd2fa14.chunk.js.LICENSE.txt"
+    "url": "/Riffle/static/js/2.7fb3a70c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de1ef51a7ba4317faae6",
-    "url": "/Riffle/static/js/main.705ee29c.chunk.js"
+    "revision": "32c5e736c2581e70af35",
+    "url": "/Riffle/static/js/main.fab1c3b7.chunk.js"
   },
   {
     "revision": "0084ea5ed7d14274a8d6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Riffle/precache-manifest.8d4f7c83d5d954219e2fc5a59480ff38.js"
+  "/Riffle/precache-manifest.da6bec976f9bd4934e3b2eea9fc727ff.js"
 );
 
 self.addEventListener('message', (event) => {
